a5b6e3a1d984009efb1d2890151c7f86f3fb2727
jyokum <jyokum@gmail.com>
2013-03-24 17:24:56 -0500

    Simplified javascript removing option to use simple vs. async and standardized on async

---
+++ js/marketo.js
@@ -0,0 +1,24 @@
+(function($) {
+  Drupal.behaviors.marketo = {
+    attach: function(context, settings) {
+      if (settings.marketo.track) {
+        jQuery.ajax({
+          url: document.location.protocol + settings.marketo.library,
+          dataType: 'script',
+          cache: true,
+          success: function() {
+            Munchkin.init(settings.marketo.key);
+            if (typeof settings.marketo.data !== 'undefined') {
+              marketoMunchkinFunction(settings.marketo.lead_type, settings.marketo.data, settings.marketo.hash);
+            }
+          }
+        });
+      }
+    }
+  }
+
+})(jQuery);
+
+function marketoMunchkinFunction(leadType, data, hash) {
+  mktoMunchkinFunction(leadType, data, hash);
+}
--- js/marketo.simple.js
+++
@@ -1,8 +0,0 @@
-Munchkin.init(Drupal.settings.marketo.key);
-marketoMunchkinFunction();
-
-function marketoMunchkinFunction() {
-  if (typeof Drupal.settings.marketo.data !== 'undefined') {
-    mktoMunchkinFunction(Drupal.settings.marketo.lead_type, Drupal.settings.marketo.data, Drupal.settings.marketo.hash);
-  }
-}
